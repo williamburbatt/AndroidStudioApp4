aa4948b4329e50ba20a7dddadd2b54a483c2aa07
williamburbatt <williamburbatt@yahoo.com>
2018-04-23 11:19:58 -0500

    Programming update

--- app/src/main/java/com/example/williamburbatt/androidstudioapp4/MainActivity.java
+++ app/src/main/java/com/example/williamburbatt/androidstudioapp4/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.williamburbatt.androidstudioapp4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
@@ -19,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
         final RadioButton lbToKilo = (RadioButton)findViewById(R.id.radLbToKilo);
         final RadioButton kiloToLb = (RadioButton)findViewById(R.id.radKiloToLb);
         final TextView result = (TextView) findViewById(R.id.txtResult);
+        Button convert = (Button) findViewById(R.id.btnConvert);
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(weightEntered < 500){
+                    convertedWeight = weightEntered/conversionRate;
+                    result.setText(""+(convertedWeight));
+                }
+            }
+        });
     }
 }
